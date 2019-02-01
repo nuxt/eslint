5f1845c1b3a9dbd971593bff91cf50ce9bc336fb
Kaz Namba <kaz.namba@gmail.com>
2019-02-01 16:53:51 +0900

    fix: avoid changing basic StandardJS rules

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -5,6 +5,6 @@ test('test basic properties of config', () => {
   expect(isObject(config.rules)).toBe(true)
 })
 
-function isObject(obj) {
+function isObject (obj) {
   return typeof obj === 'object' && obj !== null
 }
