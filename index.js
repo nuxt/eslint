f0cbdd3bf546aa2a05ec93f9a97ea87d771d5690
Alexander Lichter <manniL@gmx.net>
2019-02-26 11:25:29 +0000

    chore: lint

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ module.exports = {
   ],
   settings: {
     'import/resolver': {
-      node: { extensions: ['.js', '.mjs'] }
+      node: {extensions: ['.js', '.mjs']}
     }
   },
   rules: {
@@ -33,7 +33,7 @@ module.exports = {
     'import/no-unresolved': 0,
 
     // Allow paren-less arrow functions only when there's no braces
-    'arrow-parens': [2, 'as-needed', { requireForBlockBody: true }],
+    'arrow-parens': [2, 'as-needed', {requireForBlockBody: true}],
 
     // Allow async-await
     'generator-star-spacing': 0,
@@ -62,7 +62,7 @@ module.exports = {
     'dot-notation': 2,
 
     'no-var': 2,
-    
+
     'vue/no-parsing-error': [2, {
       'x-invalid-end-tag': false
     }],
