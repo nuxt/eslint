79c9de79c2c92209a2044d983f6007787f46ac08
Alexander Lichter <manniL@gmx.net>
2019-02-28 14:08:13 +0000

    feat: prohibit useless renaming (#28)

--- index.js
+++ index.js
@@ -63,6 +63,9 @@ module.exports = {
 
     'no-var': 2,
 
+    // No useless destructuring/importing/exporting renames
+    'no-useless-rename': 2,
+
     'vue/no-parsing-error': [2, {
       'x-invalid-end-tag': false
     }],
