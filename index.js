4029670cddcc7cd15e4ed348137abd1dc0999671
Alexander Lichter <manniL@gmx.net>
2019-04-24 13:58:47 +0200

    feat: add curated rules from unicorn plugin

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ module.exports = {
     }
   },
   rules: {
+
+    /**********************/
+    /* General Code Rules */
+    /**********************/
+
     // Enforce import order
     'import/order': 'error',
 
@@ -70,9 +75,85 @@ module.exports = {
     // No useless destructuring/importing/exporting renames
     'no-useless-rename': 'error',
 
+
+    /**********************/
+    /*   Unicorn Rules    */
+    /**********************/
+
+    // Pass error message when throwing errors
+    "unicorn/error-message": "error",
+
+    // Uppercase regex escapes
+    "unicorn/escape-case": "error",
+
+    // Use "new" for most builtins (not String, Number, ..., though)
+    "unicorn/new-for-builtins": "error",
+
+    // Array.isArray instead of instanceof
+    "unicorn/no-array-instanceof": "error",
+
+    // for-of instead traditional for loop
+    "unicorn/no-for-loop": "error",
+
+    // Prevent deprecated `new Buffer()`
+    "unicorn/no-new-buffer": "error",
+
+    // Keep regex literals safe!
+    "unicorn/no-unsafe-regex": "off",
+
+    // Use non-fraction numbers if possible, e.g. 1 instead of 1.0
+    "unicorn/no-zero-fractions": "error",
+
+    // Lowercase number formatting for octal, hex, binary (0x12 instead of 0X12)
+    "unicorn/number-literal-case": "error",
+
+    // ** instead of Math.pow()
+    "unicorn/prefer-exponentiation-operator": "error",
+
+    // includes over indexOf when checking for existence
+    "unicorn/prefer-includes": "error",
+
+    // append over appendChild
+    "unicorn/prefer-node-append": "error",
+
+    // remove over removeChild
+    "unicorn/prefer-node-remove": "error",
+
+    // querySelector instead of getElementBy...
+    "unicorn/prefer-query-selector": "error",
+
+    // Spread syntax instead of Array.from()
+    "unicorn/prefer-spread": "error",
+
+    // String methods startsWith/endsWith instead of more complicated stuff
+    "unicorn/prefer-starts-ends-with": "error",
+
+    // textContent instead of innerText
+    "unicorn/prefer-text-content": "error",
+
+    // Enforce throwing type error when throwing error while checking typeof
+    "unicorn/prefer-type-error": "error",
+
+    // Prevent abbreviations (!)
+    "unicorn/prevent-abbreviations": "error",
+
+    // Favor regex shorthands (e.g. \d instead of [0-9])
+    "unicorn/regex-shorthand": "error",
+
+    // Use new when throwing error
+    "unicorn/throw-new-error": "error",
+
+
+    /**********************/
+    /*     Vue Rules      */
+    /**********************/
+
+    // Disable template errors regarding invalid end tags
     'vue/no-parsing-error': ['error', {
       'x-invalid-end-tag': false
     }],
+
+    // Maximum 5 attributes per line instead of one
     'vue/max-attributes-per-line': ['error', {
       'singleline': 5
     }]
