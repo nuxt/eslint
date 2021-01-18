15f4f4fb0698621d06b27080daa353379b5e02e2
Brendan Mulholland <github@bmulholland.ca>
2021-01-18 11:22:29 +0100

    chore(typescript): Disable incorrect no-unused-vars check (#130)

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -7,6 +7,9 @@ module.exports = {
     parser: '@typescript-eslint/parser'
   },
   rules: {
-    '@typescript-eslint/no-unused-vars': ['error', { args: 'all', argsIgnorePattern: '^_' }]
+    '@typescript-eslint/no-unused-vars': ['error', { args: 'all', argsIgnorePattern: '^_' }],
+    // Per the docs, the root no-unused-vars should be disabled:
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unused-vars.md
+    'no-unused-vars': 'off'
   }
 }
