27e3fc1780c9242be2062f8bf6104e9a693c3c29
Xin Du (Clark) <clark.duxin@gmail.com>
2021-10-24 16:57:08 +0100

    chore: update dependencies

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -17,10 +17,10 @@ module.exports = {
   },
   settings: {
     'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
+      '@typescript-eslint/parser': ['.ts', '.tsx']
     },
     'import/resolver': {
-      typescript: {},
-    },
-  },
+      typescript: {}
+    }
+  }
 }
