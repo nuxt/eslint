13bca0f8400d5d8efc0afba307038b44ddd1e04d
Pooya Parsa <pyapar@gmail.com>
2022-05-02 13:52:27 +0200

    chore: fix lint issue

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -133,7 +133,7 @@ module.exports = {
         'pages/**/*.{js,ts,vue}',
         'layouts/**/*.{js,ts,vue}',
         'app.{js,ts,vue}',
-        'error.{js,ts,vue}',
+        'error.{js,ts,vue}'
       ],
       rules: {
         'vue/multi-word-component-names': 'off'
