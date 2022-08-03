c54d7fd648e39b139cd63ff5f96f63bd40ac4981
Felix <5724535+FelixGraf@users.noreply.github.com>
2022-08-03 20:33:52 +0200

    fix: disable vue/multi-word-component-names for files in subdirectories (#211)
    Closes #208

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -130,10 +130,10 @@ module.exports = {
   overrides: [
     {
       files: [
-        'pages/**/*.{js,ts,vue}',
-        'layouts/**/*.{js,ts,vue}',
-        'app.{js,ts,vue}',
-        'error.{js,ts,vue}'
+        '**/pages/**/*.{js,ts,vue}',
+        '**/layouts/**/*.{js,ts,vue}',
+        '**/app.{js,ts,vue}',
+        '**/error.{js,ts,vue}'
       ],
       rules: {
         'vue/multi-word-component-names': 'off'
