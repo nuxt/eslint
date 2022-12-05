f691a2a087e96396adb1bf953340717067df50b5
Daniel Roe <daniel@roe.dev>
2022-12-05 14:55:52 +0000

    chore: fix lint errors 😉

--- packages/eslint-config-next/index.js
+++ packages/eslint-config-next/index.js
@@ -1,52 +1,52 @@
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 /** @type {import('eslint').ESLint.ConfigData}  */
 module.exports = {
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
     parser: {
-      js: "espree",
-      jsx: "espree",
+      js: 'espree',
+      jsx: 'espree',
 
-      ts: "@typescript-eslint/parser",
-      tsx: "@typescript-eslint/parser",
+      ts: '@typescript-eslint/parser',
+      tsx: '@typescript-eslint/parser'
     },
-    extraFileExtensions: [".vue"],
+    extraFileExtensions: ['.vue'],
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
   env: { node: true },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:vue/vue3-recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:vue/vue3-recommended'
   ],
   overrides: [
     {
-      files: ["*.ts", "*.tsx", "*.vue"],
+      files: ['*.ts', '*.tsx', '*.vue'],
       rules: {
         // The core 'no-unused-vars' rules (in the eslint:recommended ruleset)
         // does not work with type definitions.
-        "no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars": "warn",
-      },
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'warn'
+      }
     },
     {
       // These pages are not used directly by users so they can have one-word names.
       files: [
-        "**/pages/**/*.{js,ts,vue}",
-        "**/layouts/**/*.{js,ts,vue}",
-        "**/app.{js,ts,vue}",
-        "**/error.{js,ts,vue}",
+        '**/pages/**/*.{js,ts,vue}',
+        '**/layouts/**/*.{js,ts,vue}',
+        '**/app.{js,ts,vue}',
+        '**/error.{js,ts,vue}'
       ],
-      rules: { "vue/multi-word-component-names": "off" },
+      rules: { 'vue/multi-word-component-names': 'off' }
     },
     {
       // Pages and layouts are required to have a single root element if transitions are enabled.
-      files: ["**/pages/**/*.{js,ts,vue}", "**/layouts/**/*.{js,ts,vue}"],
-      rules: { "vue/no-multiple-template-root": "error" },
-    },
-  ],
-};
+      files: ['**/pages/**/*.{js,ts,vue}', '**/layouts/**/*.{js,ts,vue}'],
+      rules: { 'vue/no-multiple-template-root': 'error' }
+    }
+  ]
+}
