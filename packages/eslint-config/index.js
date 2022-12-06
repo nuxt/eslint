bcef42b2f7ce55f828d4ebd137e3f2d8cc547366
Daniel Roe <daniel@roe.dev>
2022-12-06 11:27:57 +0000

    fix: apply recommended typescript rules to vue files as well

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -25,13 +25,41 @@ module.exports = {
   ],
   overrides: [
     {
-      files: ['*.ts', '*.tsx', '*.vue'],
+      files: ['*.ts', '*.tsx', '*.mts', '*.cts', '*.vue'],
       rules: {
         // The core 'no-unused-vars' rules (in the eslint:recommended ruleset)
         // does not work with type definitions.
         'no-unused-vars': 'off',
         '@typescript-eslint/no-unused-vars': 'warn'
       }
+    },
+    {
+      // Include typescript eslint rules in *.vue files
+      // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/eslint-recommended.ts
+      files: ['*.vue'],
+      rules: {
+        'constructor-super': 'off', // ts(2335) & ts(2377)
+        'getter-return': 'off', // ts(2378)
+        'no-const-assign': 'off', // ts(2588)
+        'no-dupe-args': 'off', // ts(2300)
+        'no-dupe-class-members': 'off', // ts(2393) & ts(2300)
+        'no-dupe-keys': 'off', // ts(1117)
+        'no-func-assign': 'off', // ts(2539)
+        'no-import-assign': 'off', // ts(2539) & ts(2540)
+        'no-new-symbol': 'off', // ts(7009)
+        'no-obj-calls': 'off', // ts(2349)
+        'no-redeclare': 'off', // ts(2451)
+        'no-setter-return': 'off', // ts(2408)
+        'no-this-before-super': 'off', // ts(2376)
+        'no-undef': 'off', // ts(2304)
+        'no-unreachable': 'off', // ts(7027)
+        'no-unsafe-negation': 'off', // ts(2365) & ts(2360) & ts(2358)
+        'no-var': 'error', // ts transpiles let/const to var, so no need for vars any more
+        'prefer-const': 'error', // ts provides better types with const
+        'prefer-rest-params': 'error', // ts provides better types with rest args over arguments
+        'prefer-spread': 'error', // ts transpiles spread to apply, so no need for manual apply
+        'valid-typeof': 'off', // ts(2367)
+      },
     },
     {
       // These pages are not used directly by users so they can have one-word names.
