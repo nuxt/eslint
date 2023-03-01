336de5dfc32de6f438e88f00a75d6b457eabad7b
Daniel Roe <daniel@roe.dev>
2023-03-01 18:54:07 +0000

    test(eslint-config): add basic test (#271)

---
+++ packages/eslint-config/test/basic.test.ts
@@ -0,0 +1,21 @@
+import { fileURLToPath } from 'node:url'
+import { it, expect, describe } from 'vitest'
+import { ESLint } from 'eslint'
+
+describe('eslint-config', () => {
+  it('loads config in eslint to validate all rule syntax is correct', async () => {
+    const cli = new ESLint({
+      useEslintrc: false,
+      overrideConfigFile: fileURLToPath(new URL('../index.js', import.meta.url)),
+    })
+
+    const code = 'const foo = 1\nconst bar = function () {}\nbar(foo)\n'
+
+    const [{ errorCount }] = await cli.lintText(code, { filePath: 'test.ts' })
+    expect(errorCount).toEqual(0)
+
+    const rules = await cli.calculateConfigForFile('test.ts')
+    expect(rules.plugins).toContain('vue')
+  })
+})
+
