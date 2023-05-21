9628a341731bf66df7b42a061f1bca88dd832d97
Daniel Roe <daniel@roe.dev>
2023-05-21 14:31:20 +0100

    fix(eslint-config): add extra file extensions to globs

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -64,18 +64,18 @@ module.exports = {
     {
       files: [
         // These pages are not used directly by users so they can have one-word names.
-        '**/pages/**/*.{js,ts,vue}',
-        '**/layouts/**/*.{js,ts,vue}',
-        '**/app.{js,ts,vue}',
-        '**/error.{js,ts,vue}',
+        '**/pages/**/*.{js,ts,jsx,tsx,vue}',
+        '**/layouts/**/*.{js,ts,jsx,tsx,vue}',
+        '**/app.{js,ts,jsx,tsx,vue}',
+        '**/error.{js,ts,jsx,tsx,vue}',
         // These files should have multiple words in their names as they are within subdirectories.
-        '**/components/*/**/*.{js,ts,vue}'
+        '**/components/*/**/*.{js,ts,jsx,tsx,vue}'
       ],
       rules: { 'vue/multi-word-component-names': 'off' }
     },
     {
       // Pages and layouts are required to have a single root element if transitions are enabled.
-      files: ['**/pages/**/*.{js,ts,vue}', '**/layouts/**/*.{js,ts,vue}'],
+      files: ['**/pages/**/*.{js,ts,jsx,tsx,vue}', '**/layouts/**/*.{js,ts,jsx,tsx,vue}'],
       rules: { 'vue/no-multiple-template-root': 'error' }
     }
   ]
