e4018838ce8a6bb32ffdd8d2eee86044f20e0874
Dávid Ďurčo <david.durco@icloud.com>
2024-03-25 09:36:37 +0100

    chore: browse + node globals

--- packages/module/src/config-addons/globals.ts
+++ packages/module/src/config-addons/globals.ts
@@ -2,6 +2,7 @@ import type { Nuxt } from '@nuxt/schema'
 import type { Unimport } from 'unimport'
 import type { ESLintConfigGenAddon } from '../types'
 import type { FlatConfig } from '@nuxt/eslint-config/flat'
+import globals from "globals";
 
 export function createAddonGlobals(nuxt: Nuxt): ESLintConfigGenAddon {
   let unimport: Unimport
@@ -19,7 +20,11 @@ export function createAddonGlobals(nuxt: Nuxt): ESLintConfigGenAddon {
           + JSON.stringify(<FlatConfig>{
             name: 'nuxt:import-globals',
             languageOptions: {
-              globals: Object.fromEntries((await unimport.getImports()).map(i => [i.as || i.name, 'readonly'])),
+              globals: {
+                ...Object.fromEntries((await unimport.getImports()).map(i => [i.as || i.name, 'readonly'])),
+                ...Object.entries(globals.browser).reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {}),
+                ...Object.entries(globals.node).reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {})
+              }
             },
           }),
         ],
