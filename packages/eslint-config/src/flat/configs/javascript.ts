d8799be4f90a8477b3794a19c95552a593b810d1
David Durco <david.durco@icloud.com>
2024-03-25 09:56:07 +0100

    fix(eslint-plugin): add browse + node globals (#361)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- packages/eslint-config/src/flat/configs/javascript.ts
+++ packages/eslint-config/src/flat/configs/javascript.ts
@@ -1,11 +1,22 @@
 // @ts-expect-error missing types
 import pluginESLint from '@eslint/js'
 import type { Linter } from 'eslint'
+import globals from 'globals'
 
 export default function javascript(): Linter.FlatConfig[] {
   return [
     {
       name: 'eslint:recommended',
+      languageOptions: {
+        globals: {
+          ...globals.browser,
+          ...globals.es2021,
+          ...globals.node,
+          document: 'readonly',
+          navigator: 'readonly',
+          window: 'readonly',
+        },
+      },
       ...pluginESLint.configs.recommended,
     },
   ]
