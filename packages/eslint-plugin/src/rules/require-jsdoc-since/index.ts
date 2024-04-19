7d509bcef12d82af027301a67a760d0c3e771db1
Luke Nelson <luke@nelson.zone>
2024-04-20 00:20:08 +0100

    Refactor to filter nodes, rather than starting with the comments and trying to work back

--- packages/eslint-plugin/src/rules/require-jsdoc-since/index.ts
+++ packages/eslint-plugin/src/rules/require-jsdoc-since/index.ts
@@ -1,6 +1,6 @@
 import { createRule } from '../utils'
 
-type MessageIds = 'default'
+type MessageIds = 'noSince' | 'noJSDoc'
 
 type Options = []
 
@@ -13,23 +13,44 @@ export const rule = createRule<MessageIds, Options>({
     },
     schema: [],
     messages: {
-      default: 'Missing `@since` tag in JSDoc comment.',
+      noSince: 'Missing `@since` tag in JSDoc comment.',
+      noJSDoc: 'Missing JSDoc comment.',
     },
   },
   defaultOptions: [],
   create: context => ({
     Program: function (node) {
-      const sourceCode = context.sourceCode
-      const comments = sourceCode.getAllComments()
+      // Filter only named and default exports
+      const onlyExported = node.body.filter(node => node.type === 'ExportNamedDeclaration' || node.type === 'ExportDefaultDeclaration')
 
-      comments.forEach((comment) => {
-        if (comment.type === 'Block' && comment.value.startsWith('*')) {
-          const jsdoc = comment.value.replace(/^\/\*\*|\*\/$/g, '')
-          console.warn(jsdoc)
-          if (!jsdoc.includes('@since')) {
+      onlyExported.forEach((exportNode) => {
+        // Filter out interfaces and type aliases
+        const declarationType = exportNode.declaration?.type
+        if (declarationType === 'TSInterfaceDeclaration' || declarationType === 'TSTypeAliasDeclaration') return
+
+        // TODO: Check if it's an override, and the function above has a valid JSDoc
+
+        // Check if the exported node has a leading comment
+        const leadingComments = context.sourceCode.getCommentsBefore(exportNode)
+        if (!leadingComments.length) {
+          // No leading comment, report missing JSDoc
+          context.report({
+            messageId: 'noJSDoc',
+            node: exportNode,
+            loc: exportNode.loc,
+          })
+        }
+        else {
+          // Check if the leading comment has a valid JSDoc
+          const hasValidJsDoc = leadingComments.some((comment) => {
+            return comment.type === 'Block' && comment.value.startsWith('*') && comment.value.includes('@since')
+          })
+
+          if (!hasValidJsDoc) {
             context.report({
-              messageId: 'default',
-              node,
+              messageId: 'noSince',
+              node: exportNode,
+              loc: leadingComments[0].loc,
             })
           }
         }
