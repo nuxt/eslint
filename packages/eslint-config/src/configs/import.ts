31bd8a0180911114811f7eb1a8aea958a70adf97
Anthony Fu <github@antfu.me>
2025-07-02 10:57:52 +0900

    feat: switch to `eslint-plugin-import-lite`, update deps

--- packages/eslint-config/src/configs/import.ts
+++ packages/eslint-config/src/configs/import.ts
@@ -1,4 +1,4 @@
-import pluginImport from 'eslint-plugin-import-x'
+import pluginImportLite from 'eslint-plugin-import-lite'
 import type { Linter } from 'eslint'
 import type { NuxtESLintConfigOptions } from '../types'
 import { resolveOptions } from '../utils'
@@ -11,18 +11,17 @@ export default function imports(options: NuxtESLintConfigOptions): Linter.Config
       name: 'nuxt/import/rules',
       plugins: {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        import: pluginImport as any,
+        import: pluginImportLite as any,
       },
       rules: {
+        'import/consistent-type-specifier-style': ['error', 'top-level'],
         'import/first': 'error',
         'import/no-duplicates': 'error',
         'import/no-mutable-exports': 'error',
         'import/no-named-default': 'error',
-        'import/no-self-import': 'error',
 
         ...resolved.features.stylistic
           ? {
-              'import/order': 'error',
               'import/newline-after-import': ['error', { count: 1 }],
             }
           : {},
